232bcc593f0b802f979ac35c1cdced1af89d72f0
ec965 <enoch965@gmail.com>
2021-06-24 01:04:40 -0700

    tinkering

--- src/main.ts
+++ src/main.ts
@@ -18,9 +18,40 @@ const findJpgs = (rootDir: string, skipDir: string[] = []): string[] => {
   return [...jpgs, ...nextJpgs].flat();
 };
 
-const jpgs = findJpgs(".", ["node_modules"]);
-console.log(jpgs);
+const createDirPath = (dirPath: string): string[] => {
+  const outPath = [...dirPath.split("/")];
+  let currentDir = "";
+  for (let i = 0; i < outPath.length - 1; i++) {
+    currentDir += outPath[i];
+    currentDir += "/";
+    mkDir(currentDir);
+  }
+  return outPath;
+};
 
-// sharp("pics/hc2.JPG")
-//   .resize(300,200)
-//   .toFile("out.jpg", (err) => err && console.error(err));
+const mkDir = (dir: string) => !fs.existsSync(dir) && fs.mkdirSync(dir);
+
+function main() {
+  const output = "out";
+  // 4:3
+  const width = 500;
+  const height = 375;
+
+  mkDir(output);
+
+  findJpgs(".", ["node_modules", "out"]).forEach(async (jpg) => {
+    const outPath = createDirPath(`${output}/${jpg}`);
+    console.time(jpg);
+    try {
+      await sharp(jpg)
+        .resize({ width, height, fit: "contain", background: "#444"})
+        .normalise()
+        .toFile(path.join(...outPath));
+    } catch (err) {
+      console.error(err);
+    }
+    console.timeLog(jpg);
+  });
+}
+
+main();
