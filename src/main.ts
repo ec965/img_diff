b339fc82a99dcef4ab8e976bfd2263c10a2e75b0
ec965 <enoch965@gmail.com>
2021-06-27 18:41:42 -0700

    turn into cli

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,29 @@
+#!/usr/bin/env node
 import sharp from "sharp";
 import fs from "fs";
 import path from "path";
+import os from "os";
 import minimist from "minimist";
 
 const isJpg = (name: string): boolean => Boolean(name.match(/.*\.(jpg|JPG)$/m));
 
-const findJpgs = (rootDir: string, skipDir: string[] = []): string[] => {
+/**
+ * Find all jpg files in the root directory
+ * @param rootDir
+ * @param skipDir directories to skip
+ * @returns
+ */
+const findJpgs = (
+  rootDir: string,
+  skipDir: string[] = [],
+  recursive = false
+): string[] => {
   const files = fs.readdirSync(rootDir);
   const jpgs = files
     .filter((file) => isJpg(file))
     .map((file) => path.join(rootDir, file));
+  if (!recursive) return jpgs;
+
   const nextJpgs = files
     .filter((file) => fs.statSync(path.join(rootDir, file)).isDirectory())
     .filter((dir) => !skipDir.includes(dir))
@@ -18,35 +32,104 @@ const findJpgs = (rootDir: string, skipDir: string[] = []): string[] => {
   return [...jpgs, ...nextJpgs];
 };
 
+/**
+ * create folders in the specified path name
+ * @param dirPath
+ * @returns
+ */
 const createDirPath = (...dirPath: string[]): void =>
   dirPath.forEach(
-    (_, i, arr) =>
-      i < arr.length - 1 && mkDir(path.join(...dirPath.slice(0, i + 1)))
+    (_, i, arr) => i < arr.length - 1 && mkDir(path.join(...dirPath))
   );
 
+/**
+ * make a directory if it doesn't exist
+ * @param dir directory path
+ * @returns
+ */
 const mkDir = (dir: string) => !fs.existsSync(dir) && fs.mkdirSync(dir);
 
-// entry
-const argv = minimist(process.argv.slice(2));
-const output = "out";
-// 4:3
-const width = argv.w ?? 500;
-const height = width * (3 / 4);
-const background = argv.b ?? "#ffffff";
+export interface Config {
+  width: number;
+  height: number;
+  aspectRatio: number;
+  ratioUseHeight: boolean;
+  ratioUseWidth: boolean;
+  background: string;
+  omitDir: string[];
+}
 
-mkDir(output);
+const defaultConfig: Config = {
+  width: 500,
+  height: 500,
+  aspectRatio: 4 / 3,
+  ratioUseHeight: false,
+  ratioUseWidth: true,
+  background: "#fff",
+  omitDir: [],
+};
 
-findJpgs(".", ["node_modules", "out"]).forEach(async (jpg) => {
-  const outPath = `${output}/${jpg}`.split("/");
-  createDirPath(...outPath);
-  console.time(jpg);
-  try {
-    await sharp(jpg)
-      .resize({ width, height, fit: "contain", background })
-      .normalise()
-      .toFile(path.join(...outPath));
-  } catch (err) {
-    console.error(err);
+function main() {
+  // get the config file
+  const configFile = path.join(
+    os.homedir(),
+    ".config",
+    "img-diff",
+    "config.json"
+  );
+  if (!fs.existsSync(configFile)) {
+    createDirPath(os.homedir(), ".config", "img-diff");
+    fs.writeFileSync(configFile, JSON.stringify(defaultConfig));
   }
-  console.timeLog(jpg);
-});
+
+  let config: Config = { ...defaultConfig, ...require(configFile) };
+
+  // validate config
+  if (config.ratioUseHeight && config.ratioUseWidth)
+    throw new Error(
+      "Only one direction can be used to scale the aspect ratio."
+    );
+
+  if (config.ratioUseHeight)
+    config.width = Math.floor(config.height * config.aspectRatio);
+  if (config.ratioUseWidth)
+    config.height = Math.floor(config.width * (1 / config.aspectRatio));
+
+  // entry
+
+  const argv = minimist(process.argv.slice(2));
+  if (!argv.o) throw new Error("use -o to specify an output directory");
+  const outDir = path.join(process.cwd(), argv.o);
+  if (!argv.i) throw new Error("use -i to specify an input directory");
+  const inDir = path.join(process.cwd(), argv.i);
+  const recursive = Boolean(argv.r);
+
+  mkDir(outDir);
+
+  findJpgs(inDir, config.omitDir, recursive).forEach(async (jpg) => {
+    const jpgName = jpg.replace(inDir, "");
+    const jpgOutDir = outDir.split("/");
+    createDirPath("/", ...jpgOutDir);
+
+    console.time(jpg);
+    try {
+      await sharp(jpg)
+        .resize({
+          width: config.width,
+          height: config.height,
+          fit: "contain",
+          background: config.background,
+        })
+        .normalise()
+        .toFile(path.join("/", ...jpgOutDir, jpgName));
+    } catch (err) {
+      console.error(err);
+    }
+    console.timeLog(jpg);
+  });
+}
+try {
+  main();
+} catch (error) {
+  console.error(error.message);
+}
